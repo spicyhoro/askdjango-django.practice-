ecb8dc78ea6236e32a733389be4c4eed19eccef3
spicyhoro <spicyhoro777@gmail.com>
2019-11-26 18:27:37 +0900

    로그인, 로그아웃, 그페이로 돌아오기

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from accounts import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.conf import settings
 
 urlpatterns = [
-    path('profile/', views.profile),
+    path('profile/', views.profile, name='profile'),
     path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page=settings.LOGIN_URL), name='logout'),
 ]
